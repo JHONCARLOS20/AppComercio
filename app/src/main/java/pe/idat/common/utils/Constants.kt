c62c0a12bc432215953014d447d2bac4c966fc36
JHONCARLOS20 <jhoncarlos_compras_123_@hotmail.com>
2022-08-26 00:16:20 -0500

    Semana 17

--- app/src/main/java/pe/idat/common/utils/Constants.kt
+++ app/src/main/java/pe/idat/common/utils/Constants.kt
@@ -2,8 +2,8 @@ package pe.idat.common.utils
 
 object Constants
 {
-    const val COMERCIOS_URL="https://tarea.free.beeceptor.com/"
-    const val GET_ALL_PATH="/my/api/path"
+    const val COMERCIOS_URL="https://trabajo.free.beeceptor.com/"
+    const val GET_ALL_PATH="my/api/path"
 
     const val STATUS_PROPERTY="status" //(0 = tabla vacia, 1 = tabla al menos con un dato, 2 = error)
     const val COMERCIOS_PROPERTY="comercios"
